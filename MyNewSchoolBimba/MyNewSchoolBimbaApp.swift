a6c7e799c940d426ac037294d1ebd7464d24bde4
Giacomo Angione <angi1g@gmail.com>
2024-02-05 12:26:43 +0100

    Initial Commit

---
+++ MyNewSchoolBimba/MyNewSchoolBimbaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewSchoolBimbaApp.swift
+//  MyNewSchoolBimba
+//
+//  Created by Barbara on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MyNewSchoolBimbaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
